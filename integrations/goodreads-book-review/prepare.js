4be9eddf3098e6f5dc3bdfe1c7faf89c5e591e1e
Andrew Jenkins <andrew.jenkins88@gmail.com>
2024-11-06 16:01:06 -0700

    testing new changes

--- integrations/goodreads-book-review/prepare.js
+++ integrations/goodreads-book-review/prepare.js
@@ -1,6 +1,8 @@
 function prepare(ctx, manifest) {
   const cookies = ctx.cookies;
 
+  manifest.appendDebugLog(1);
+
   const ccsid = cookies["ccsid"];
   const locale = cookies["locale"];
   const sessionId = cookies["session-id"];
@@ -15,6 +17,36 @@ function prepare(ctx, manifest) {
   const atMain = cookies["at-main"];
   const sessAtMain = cookies["sess-at-main"];
   const sessionId2 = cookies["_session_id2"];
+
+  manifest.appendDebugLog(2);
+  manifest.appendDebugLog("ccsid");
+  manifest.appendDebugLog(JSON.stringify(ccsid));
+  manifest.appendDebugLog("locale");
+  manifest.appendDebugLog(JSON.stringify(locale));
+  manifest.appendDebugLog("sessionId");
+  manifest.appendDebugLog(JSON.stringify(sessionId));
+  manifest.appendDebugLog("lcMain");
+  manifest.appendDebugLog(JSON.stringify(lcMain));
+  manifest.appendDebugLog("ubidMain");
+  manifest.appendDebugLog(JSON.stringify(ubidMain));
+  manifest.appendDebugLog("likelyHasAccount");
+  manifest.appendDebugLog(JSON.stringify(likelyHasAccount));
+  manifest.appendDebugLog("blockingSignInInterstitial");
+  manifest.appendDebugLog(JSON.stringify(blockingSignInInterstitial));
+  manifest.appendDebugLog("sessionIdTime");
+  manifest.appendDebugLog(JSON.stringify(sessionIdTime));
+  manifest.appendDebugLog("JSESSIONID");
+  manifest.appendDebugLog(JSON.stringify(JSESSIONID));
+  manifest.appendDebugLog("sessionToken");
+  manifest.appendDebugLog(JSON.stringify(sessionToken));
+  manifest.appendDebugLog("xMain");
+  manifest.appendDebugLog(JSON.stringify(xMain));
+  manifest.appendDebugLog("atMain");
+  manifest.appendDebugLog(JSON.stringify(atMain));
+  manifest.appendDebugLog("sessAtMain");
+  manifest.appendDebugLog(JSON.stringify(sessAtMain));
+  manifest.appendDebugLog("sessionId2");
+  manifest.appendDebugLog(JSON.stringify(sessionId2));
 
   if (
     !ccsid ||
@@ -35,6 +67,8 @@ function prepare(ctx, manifest) {
     return false;
   }
 
+  manifest.appendDebugLog(3);
+
   const cookie = [
     "ccsid=" + ccsid.value,
     "locale=" + locale.value,
@@ -52,7 +86,10 @@ function prepare(ctx, manifest) {
     "_session_id2=" + sessionId2.value,
   ].join("; ");
 
+  manifest.appendDebugLog(4);
+
   manifest.request.set("Cookie", cookie);
 
+  manifest.appendDebugLog(5);
   return true;
 }

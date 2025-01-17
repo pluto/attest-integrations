2fa052222ef12a7e6c916d34346f637ff18bcbd8
Andrew Jenkins <andrew.jenkins88@gmail.com>
2025-01-17 13:40:14 -0700

    Attempt to use old.reddit

--- integrations/reddit-user-karma/prepare.js
+++ integrations/reddit-user-karma/prepare.js
@@ -2,60 +2,20 @@ function prepare(ctx, manifest) {
   const cookies = ctx.cookies;
   const doc = ctx.doc;
 
-  // Set all headers at once
-  if (cookies["token_v2"]) {
-    manifest.request.set("authToken", cookies["token_v2"].value);
-  }
-
   try {
-    // Check if the page is using the old Reddit design
-    const isOldReddit = !!doc.querySelector(
-      "button.redesign-beta-optin#redesign-beta-optin-btn"
-    );
-    let userId = "";
-
-    if (isOldReddit) {
-      // Old Reddit logic
-      manifest.appendDebugLog("Using old Reddit logic");
-
-      // Check for desktop version user ID
-      let userLink = doc.querySelector('span.user > a[href*="/user/"]');
-      if (userLink) {
-        userId = userLink
-          .getAttribute("href")
-          .split("/user/")[1]
-          .replace("/", "");
-      } else {
-        // Check for mobile version user ID
-        userLink = doc.querySelector('a[href*="/user/"]');
-        if (userLink) {
-          userId = userLink
-            .getAttribute("href")
-            .split("/user/")[1]
-            .replace("/", "");
-        }
-      }
-    } else {
-      // New Reddit logic
-      manifest.appendDebugLog("Using new Reddit logic");
-
-      const desktopDrawer = doc.querySelector("#user-drawer-content a");
-      if (desktopDrawer) {
-        userId = desktopDrawer.getAttribute("href").split("/")[2];
-      } else {
-        const mobileDrawer = doc.querySelector("#hui-user-drawer a");
-        if (mobileDrawer) {
-          userId = mobileDrawer.getAttribute("href").split("/")[2];
-        }
-      }
+    // Auth Token
+    if (cookies["token_v2"]) {
+      manifest.request.set("authToken", cookies["token_v2"].value);
     }
 
-    // If userId is found, return it
-    if (userId) {
-      manifest.request.set("userId", userId);
+    // User ID
+    const userLink = doc.querySelector('span.user > a[href*="/user/"]');
+    if (userLink) {
+      manifest.request.set(
+        "userId",
+        userLink.getAttribute("href").split("/user/")[1].replace("/", "")
+      );
     }
-
-    manifest.appendDebugLog(manifest.request.get("body").variables.name);
 
     return (
       !manifest.request.get("body").variables.name.includes("<%") &&

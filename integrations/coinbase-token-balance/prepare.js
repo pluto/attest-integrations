0266ce76cb5130dd62a45914aeaf438cf6c2f8dd
Andrew Jenkins <andrew.jenkins88@gmail.com>
2025-02-25 10:04:07 -0700

    Update prepare.js

--- integrations/coinbase-token-balance/prepare.js
+++ integrations/coinbase-token-balance/prepare.js
@@ -2,16 +2,21 @@ function prepare(ctx, manifest) {
   const cookies = ctx.cookies;
 
   try {
+    manifest.appendDebugLog("In");
     // Cookie
     if (cookies["unified-session-manager-cookie"]) {
+      manifest.appendDebugLog("In2")
       manifest.request.set(
         "cookie",
         cookies["unified-session-manager-cookie"].value
       );
+      manifest.appendDebugLog("In3")
     }
     manifest.appendDebugLog(manifest.request.getHeader("cookie"));
     return !manifest.request.getHeader("cookie").includes("<%");
   } catch (e) {
+    manifest.appendDebugLog("error");
+    manifest.appendDebugLog(e.message);
     console.error("Error in prepare.js:", e);
     return false;
   }

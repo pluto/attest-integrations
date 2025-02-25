2baa52ec3c764bb9116730e160c8e33619814910
Andrew Jenkins <andrew.jenkins88@gmail.com>
2025-02-25 11:11:59 -0700

    Remove Debug Logs

--- integrations/coinbase-token-balance/prepare.js
+++ integrations/coinbase-token-balance/prepare.js
@@ -2,21 +2,16 @@ function prepare(ctx, manifest) {
   const cookies = ctx.cookies;
 
   try {
-    manifest.appendDebugLog("In");
     // Cookie
     if (cookies["unified-session-manager-cookie"]) {
-      manifest.appendDebugLog("In2")
       manifest.request.set(
         "cookie",
         cookies["unified-session-manager-cookie"].value
       );
-      manifest.appendDebugLog("In3")
     }
-    manifest.appendDebugLog(manifest.request.getHeader("Cookie"));
+    
     return !manifest.request.getHeader("Cookie").includes("<%");
   } catch (e) {
-    manifest.appendDebugLog("error");
-    manifest.appendDebugLog(e.message);
     console.error("Error in prepare.js:", e);
     return false;
   }

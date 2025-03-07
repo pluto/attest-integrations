859d218166a306e4137dd604cb43377bbeb80160
Andrew Jenkins <andrew.jenkins88@gmail.com>
2025-03-07 14:10:59 -0700

    Debugging

--- integrations/linkedin-account-ownership/v3/prepare.js
+++ integrations/linkedin-account-ownership/v3/prepare.js
@@ -1,18 +1,22 @@
 function prepare(ctx, manifest) {
   const cookies = ctx.cookies;
 
+  manifest.appendDebugLog("Cookies: " + JSON.stringify(cookies));
   try {
     if (cookies["JSESSIONID"]) {
+      manifest.appendDebugLog("JSESSIONID: " + cookies["JSESSIONID"].value);
       manifest.request.set("csrfToken", cookies["JSESSIONID"].value);
     }
 
     if (cookies["li_at"]) {
+      manifest.appendDebugLog("li_at: " + cookies["li_at"].value);
       manifest.request.set("authToken", cookies["li_at"].value);
     }
 
+    manifest.appendDebugLog("Cookie: " + manifest.request.getHeader("Cookie"));
     return !manifest.request.getHeader("Cookie").includes("<%");
   } catch (e) {
-    console.error("Error in prepare.js:", e);
+    manifest.appendDebugLog("Error in prepare.js:", JSON.stringify(e));
     return false;
   }
 }
